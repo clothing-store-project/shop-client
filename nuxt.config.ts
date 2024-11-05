56e48c16f753cd3be7ecb4ffdbab9b64e6b1a909
Nguyen Huu Binh <kiaisoft.binh.nguyen@gmail.com>
2024-11-05 23:27:25 +0700

    config CI/CD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,33 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     // head
     head: {
       title: 'Shop Ecommerce',
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
         {
           hid: 'description',
           name: 'description',
           content: 'Shop Ecommerce',
         },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+      link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
     }
   },
-
+  
   devtools: {
     enabled: false
   },
-
+  
   typescript: {
     strict: false
   },
-
+  
   // css
-  css: ['~/assets/scss/index.scss','animate.css'],
-
+  css: ['~/assets/scss/index.scss', 'animate.css'],
+  
   // build modules
   modules: [
     '@vueuse/nuxt',
@@ -35,21 +36,21 @@ export default defineNuxtConfig({
     '@element-plus/nuxt',
     '@nuxtjs/color-mode'
   ],
-
+  
   // vueuse
   vueuse: {
     ssrHandlers: true,
   },
-
+  
   routeRules: {
-    '/': { prerender: true }
+    '/': {prerender: true}
   },
-
+  
   // colorMode
   colorMode: {
     classSuffix: '',
   },
-
+  
   unocss: {
     uno: true,
     attributify: true,
@@ -57,19 +58,19 @@ export default defineNuxtConfig({
       scale: 1.2,
     },
   },
-
+  
   vite: {
     css: {
       preprocessorOptions: {
-        scss: { api: 'modern-compiler' },
+        scss: {api: 'modern-compiler'},
       }
     },
   },
-
+  
   elementPlus: {
     icon: 'ElIcon',
     themes: ['dark'],
   },
-
+  
   compatibilityDate: '2024-10-30',
 })
